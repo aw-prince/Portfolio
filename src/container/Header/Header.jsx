5dd512b84bbae0b43425629957661ba495230908
aw-prince <awprince.ynu@gmail.com>
2022-11-04 13:01:20 +0600

    Header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,6 +3,16 @@ import { motion } from "framer-motion";
 import { images } from "../../constants";
 
 import "./Header.scss";
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
 
 export default function Header() {
   return (
@@ -26,6 +36,32 @@ export default function Header() {
             <p className="p-text">Product Manager</p>
           </div>
         </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="profile_bg" />
+          </div>
+        ))}
       </motion.div>
     </div>
   );
